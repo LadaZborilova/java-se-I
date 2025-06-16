b8497e1749a7d31b4f4565c9f047610fc4f21d3e
LadaZbořilová <ladazborilova97@gmail.com>
2025-06-16 14:52:04 +0200

    feat: add manual and random array

--- src/main/java/pfarray/Main.java
+++ src/main/java/pfarray/Main.java
@@ -1,4 +1,19 @@
 package pfarray;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] randomArray = PFArray.generateRandomArray();
+        System.out.println("Random array:");
+        for (int num : randomArray) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println("\n");
+
+        int[] manualArray = PFArray.generateManualArray();
+        System.out.println("Manual array:");
+        for (int num : manualArray) {
+            System.out.print(num + " ");
+        }
+    }
 }
--- src/main/java/pfarray/PFArray.java
+++ src/main/java/pfarray/PFArray.java
@@ -1,4 +1,38 @@
 package pfarray;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class PFArray {
+
+    public static int[] generateRandomArray() {
+        Random r = new Random();
+        int min = 3;
+        int max = 20;
+        int length = r.nextInt(max - min + 1) + min;
+
+        int[] array = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            array[i] = r.nextInt(201) - 100;
+        }
+
+        return array;
+    }
+
+    public static int[] generateManualArray() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Elements:");
+        int size = scanner.nextInt();
+
+        int[] manualArray = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Number " + (i + 1) + ": ");
+            manualArray[i] = scanner.nextInt();
+        }
+
+        return manualArray;
+    }
 }
