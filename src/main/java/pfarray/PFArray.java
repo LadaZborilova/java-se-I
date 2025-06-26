6636542d344c9b09d2f51692d73bc77178b472d6
LadaZbořilová <ladazborilova97@gmail.com>
2025-06-26 20:34:27 +0200

    Revert "fix: test git commit -a -m"
    This reverts commit a22d96636cea05b5e2059ced849941f02b11dfe1.

--- src/main/java/pfarray/PFArray.java
+++ src/main/java/pfarray/PFArray.java
@@ -44,7 +44,7 @@ public class PFArray {
     }
     public static int findMax (int[] array){
         int max = array[0];
-        for (int i = 2; i < array.length; i++){
+        for (int i = 1; i < array.length; i++){
             if(array[i] > max)
                 max = array[i];
         }
